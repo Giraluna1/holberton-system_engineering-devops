668df20e37839dbb691befe05c84cfb2616c5a4b
Giraluna1 <lunagolo@hotmail.com>
2021-04-05 01:08:47 -0500

    Delete cwd and create

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,9 +1,6 @@
 # Resource EXEC for execute a command
 exec { 'pkill':
-path    => '/usr/bin:/usr/sbin:/bin',
+path    => '/usr/bin',
 command => 'pkill -KILL killmenow',
-cwd     => '/home/luna/holberton-system_engineering-devops/0x0A-configuration_management',
-creates => '/home/luna/holberton-system_engineering-devops/0x0A-configuration_management/killmenow',
-
 
 }
