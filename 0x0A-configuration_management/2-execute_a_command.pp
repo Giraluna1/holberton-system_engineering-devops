784d782a032f83ccebfcbb28d60adb0c36c7833f
Giraluna1 <lunagolo@hotmail.com>
2021-04-05 01:04:29 -0500

    Kill a process from puppet

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,9 @@
 # Resource EXEC for execute a command
-exec { 'Killer':
+exec { 'pkill':
+path    => '/usr/bin:/usr/sbin:/bin',
 command => 'pkill -KILL killmenow',
 cwd     => '/home/luna/holberton-system_engineering-devops/0x0A-configuration_management',
 creates => '/home/luna/holberton-system_engineering-devops/0x0A-configuration_management/killmenow',
-path    => '/usr/bin:/usr/sbin:/bin',
+
 
 }
