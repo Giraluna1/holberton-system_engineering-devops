5437834da342313ce6c7ddc56d210916d54240cd
Giraluna1 <lunagolo@hotmail.com>
2021-04-05 00:02:16 -0500

    New project is going

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Create a file .pp
+file { 'holberton':
+    ensure  => file,
+    path    => '/tmp/holberton'
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet'
+
+}
