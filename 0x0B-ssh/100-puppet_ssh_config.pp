1d642eb4bdfac9fac056070681a3677faaa4359c
Giraluna1 <lunagolo@hotmail.com>
2021-04-05 23:34:45 -0500

    Add copy 4 puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+file_line { 'Use the private key':
+ensure => present,
+path   => '/etc/ssh/ssh_config',
+line   => '   IdentityFile ~/.ssh/holberton',
+
+}
+
+file_line { 'authenticate without a pwd':
+ensure => present,
+path   => '/etc/ssh/ssh_config',
+line   => '     PasswordAuthentication no',
+
+}
