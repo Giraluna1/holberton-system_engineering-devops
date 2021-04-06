5c0c2770e1af144878310b2ed99d0961a40f2d3e
Giraluna1 <lunagolo@hotmail.com>
2021-04-05 23:49:29 -0500

    presen into simple cuotes

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # Client configuration file (w/puppet)
 # Use Identity file and not authentication
 
-file_line { 'Use private key':
+file_line { 'Declare identity file':
     ensure => 'present',
     path   => '/etc/ssh/ssh_config',
     line   => 'IdentityFile ~/.ssh/holberton',
 }
 
-file_line { 'authenticate without a pwd':
+file_line { 'Turn off passwd auth':
     ensure => 'present',
     path   => '/etc/ssh/ssh_config',
     line   => 'PasswordAuthentication no',
