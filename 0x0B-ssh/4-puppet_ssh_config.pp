91aeabf417e9ee38a587c508e4d672efcea9c7f6
Giraluna1 <lunagolo@hotmail.com>
2021-04-05 23:41:06 -0500

    Fixx layout

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,15 +2,13 @@
 # Use Identity file and not authentication
 
 file_line { 'Use the private key':
-ensure => present,
-path   => '/etc/ssh/ssh_config',
-line   => '   IdentityFile ~/.ssh/holberton',
-
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '   IdentityFile ~/.ssh/holberton',
 }
 
 file_line { 'authenticate without a pwd':
-ensure => present,
-path   => '/etc/ssh/ssh_config',
-line   => '     PasswordAuthentication no',
-
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '     PasswordAuthentication no',
 }
