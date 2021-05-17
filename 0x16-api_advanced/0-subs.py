133203efb504d68cba33737117b272011f9d2249
Giraluna1 <lunagolo@hotmail.com>
2021-05-17 14:09:11 -0500

    Add user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,11 +3,14 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """ Returns the number of subscribes """
 
     if subreddit:
-        r_subs = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit))
+        user_agent = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0'}
+        r_subs = requests.get(
+            "https://www.reddit.com/r/{}/about.json".format(subreddit), headers=user_agent)
         json_subreddit = r_subs.json()
         return json_subreddit.get('data').get("subscribers")
     else:
