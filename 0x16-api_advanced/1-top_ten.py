2ec45571bd57b44ff1388342d742cd68fe1934d6
Giraluna1 <lunagolo@hotmail.com>
2021-05-17 14:46:18 -0500

    Find the first 10 post

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""  Module TOP TEN """
+
+import requests
+
+
+def top_ten(subreddit):
+    """  prints the titles of the first 10 hot posts listed for a given subreddit. """
+
+    user_agent = {'user-agent': 'my-app/0.0.1'}
+    r_subs = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(
+            subreddit), headers=user_agent)
+    if r_subs.status_code != 200:
+        print(None)
+    else:
+        json_subreddit = r_subs.json()
+        children = json_subreddit.get('data').get('children')
+        for dictionaries in children[:10]:
+            tittle = dictionaries.get("data").get("title")
+            print(tittle)
