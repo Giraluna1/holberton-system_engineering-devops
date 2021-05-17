689beab7f8f842562060dd115c59268d9ef543ad
Giraluna1 <lunagolo@hotmail.com>
2021-05-17 14:11:39 -0500

    fixed pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,9 +8,10 @@ def number_of_subscribers(subreddit):
     """ Returns the number of subscribes """
 
     if subreddit:
-        user_agent = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0'}
+        user_agent = {'user-agent': 'my-app/0.0.1'}
         r_subs = requests.get(
-            "https://www.reddit.com/r/{}/about.json".format(subreddit), headers=user_agent)
+            "https://www.reddit.com/r/{}/about.json".format(
+                subreddit), headers=user_agent)
         json_subreddit = r_subs.json()
         return json_subreddit.get('data').get("subscribers")
     else:
