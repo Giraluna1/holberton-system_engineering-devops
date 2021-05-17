3de38fc838e36c1b90d211444f9654810d3f0513
Giraluna1 <lunagolo@hotmail.com>
2021-05-17 14:19:12 -0500

    Fix control

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,12 @@ import requests
 def number_of_subscribers(subreddit):
     """ Returns the number of subscribes """
 
-    if subreddit:
-        user_agent = {'user-agent': 'my-app/0.0.1'}
-        r_subs = requests.get(
-            "https://www.reddit.com/r/{}/about.json".format(
-                subreddit), headers=user_agent)
+    user_agent = {'user-agent': 'my-app/0.0.1'}
+    r_subs = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(
+            subreddit), headers=user_agent)
+    if r_subs.status_code != 200:
+        return 0
+    else:
         json_subreddit = r_subs.json()
         return json_subreddit.get('data').get("subscribers")
-    else:
-        return 0
